be7fb9ad848b48e71b11e5abde060f0dbc79280e
Daniel Santos <danieltisantos@gmail.com>
2022-10-24 18:42:37 -0300

    Adicionado conector mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
+import { EnderecoModule } from './endereco/endereco.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from './usuario/entities/usuario.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    UsuarioModule,
+    EnderecoModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'usuarios',
+      entities: [Usuario],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
